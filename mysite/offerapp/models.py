79c6e0d60893ec1d79c35406f27911bc85d4b03c
noor atyat <nooratyat92@gmail.com>
2017-05-11 20:14:17 +0300

    make DB models

--- mysite/offerapp/models.py
+++ mysite/offerapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Offer(models.Model):
+	company_name=models.CharField(max_length=200)
+	discription=models.CharField(max_length=200)
+	date=models.DateTimeField('date published')
+
+class company(models.Model):
+	offer=models.Foreignkey(Offer,on_delete=models.CASCADE)
+	name=models.CharField(max_length=200)
+	email=models.CharField(max_length=200)
+	password=models.CharField(max_length=200)
